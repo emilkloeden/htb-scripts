394a832bf608e052b955c1fc194c9f21f1653003
Emil Kloeden <emilkloeden@users.noreply.github.com>
2021-07-27 23:28:49 +0930

    Update w.sh
    Corrected behaviour of nmap as to ports (tr now needed to replace spaces, not newlines)

--- w.sh
+++ w.sh
@@ -15,7 +15,7 @@ MACHINE_NAME=$1
 MACHINE_DIR="$WINDOWS_DIR/$MACHINE_NAME"
 NMAP_DIR="$MACHINE_DIR/nmap"
 PORTS_FILE="$MACHINE_DIR/ports"
-NMAP_FILE="$NMAP_DIR/default.nmap"
+NMAP_FILE="$NMAP_DIR/default"
 
 # Get open/filtered ports
 echo -e "[+] Creating ${LIGHT_GREEN}$NMAP_DIR${NO_COLOR} if it doesn't exist..."
@@ -23,13 +23,11 @@ mkdir -p $NMAP_DIR
 echo "[+] Running fast nmap scan to discover open ports (this may take a while)..."
 ports=$(nmap -p- --min-rate=1000 -Pn -T4 $MACHINE_IP | grep '^[0-9]' | cut -d '/' -f 1)
 
-comma_separated_ports_list=$(echo $ports | tr '\n' ',' | sed s/,$//)
-echo -e "[+] Discovered ports: ${LIGHT_GREEN}$comma_separated_ports_list${NO_COLOR}. Saving to ${LIGHT_GREEN}$PORTS_FILE${NO_COLOR}"
-printf "%s $ports"
+comma_separated_ports_list=$(echo $ports | tr ' ' ',' | sed s/,$//)
+echo -e "[+] Discovered ports: ${LIGHT_GREEN}$comma_separated_ports_list${NO_COLOR}. Saving to ${LIGHT_GREEN}$PORTS_FILE${NO_COLOR}."
 printf "%s $ports\n" > $PORTS_FILE
 
 # Run a complete nmap scan
 echo -e "[+] Running default nmap scripts and version scan. Saving output to ${LIGHT_GREEN}$NMAP_FILE${NO_COLOR}."
-nmap -v -p $comma_separated_ports_list -Pn -sC -sV $MACHINE_IP -o $NMAP_FILE
-
+nmap -v -p $comma_separated_ports_list -Pn -sC -sV $MACHINE_IP -oA $NMAP_FILE
 
